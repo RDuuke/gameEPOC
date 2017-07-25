98a4eb217fcdf48fcd9c420c9e2268fb6d2ca6d4
Juan Duque <juuanduuke@gmail.com>
2017-07-24 20:23:09 -0500

    Next level the game

--- js/template.js
+++ js/template.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
     var answers = document.getElementsByClassName('answers');
     var boxAnswers = document.getElementsByClassName('box-answer');
-    var elementMove; 
+    var elementMove;
     for(i in answers){
         var answer = answers[i];
         if (typeof answer.style != "undefined"){
@@ -20,47 +20,3 @@ $(document).ready( function(){
     }
     
 });
-
-function dragIniciado(e)
-{
-    this.style.background = "blue";
-    elementMove = this;
-    var father = document.createElement('p');
-    var clone = this.cloneNode(true);
-    if (this.getAttribute('data-answer') != 1) {
-        clone.style.background = '#F8BE4F';
-    }else{
-        clone.style.background = '#84D9B5';
-    }
-    father.appendChild(clone);
-    e.dataTransfer.setData('text', father.innerHTML);
-}
-function dragFinalizado(e){
-    
-    this.style.background = "red";
-};
-
-function dragSobreContainer(e)
-{
-    e.preventDefault();
-    this.classList.add('over');
-    return false;
-}
-
-function dragSalioContainer(e)
-{
-    e.preventDefault();
-    this.classList.remove('over');
-    return false;
-}
-
-function manejarDrop(e)
-{
-    e.preventDefault();
-    if (this.getAttribute('data-box') == elementMove.getAttribute('data-answer')) {
-        var datos = e.dataTransfer.getData('text');
-        this.innerHTML += datos;
-        elementMove.parentNode.removeChild(elementMove);
-    }
-    this.classList.remove('over');
-}
